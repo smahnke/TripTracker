6195b28fb989c6bc2ee395f7a393dd8eaa6aca75
Steph Mahnke <mahnkes1@msu.edu>
2021-06-05 10:55:18 -0600

    added user and trip controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,44 @@
 class UsersController < ApplicationController
   def index
+    @users = User.All
+    render component: 'Users', props:{ users: @users }
   end
 
   def show
+    @user = User.find(params[:id])
+    render component: 'User', props: {user: @user}
   end
 
   def new
+    @user = User.new
+    render component: 'UserNew', props: { user: @user }
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path
+    else
+      render component: 'UserNew', props: { user: @user }
+    end
   end
 
   def edit
+    @user = User.find(params[:id])
+    render component: 'UserEdit', props: { user: @user }
   end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to root_path
+    else
+      render component: 'UserEdit', props: { user: @user }
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:email, :password)
+    end
 end
