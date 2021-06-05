1a7ad758f0518cc5c5709914a77bdcc5dd3fa2ee
Steph Mahnke <mahnkes1@msu.edu>
2021-06-05 16:16:24 -0600

    added navbar

--- Gemfile
+++ Gemfile
@@ -27,8 +27,8 @@ gem 'jbuilder', '~> 2.7'
 gem 'bootsnap', '>= 1.4.4', require: false
 
 gem 'react-rails'
-
-gem 'bcrypt'
+gem 'bootstrap'
+gem 'jquery-rails'
 
 group :development, :test do
   gem 'pry'
--- app/javascript/components/Navbar.js
+++ app/javascript/components/Navbar.js
@@ -1,11 +1,34 @@
 import React from 'react';
 
-const Navbar = () => (
+// const Navbar = () => (
+//   <>
+//     <a href='/'>Home</a>
+//     {'  '}
+//     <a href='/users'>Users</a>
+//   </>
+// )
+
+const Navbar=() => (
   <>
-    <a href='/'>Home</a>
-    {'  '}
-    <a href='/users'>Users</a>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">TRIPTRACKER:</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="/">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/users">Users</a>
+          </li>
+        </ul>
+      </div>
+      </div>
+    </nav>
   </>
-)
+  )
 
 export default Navbar;
